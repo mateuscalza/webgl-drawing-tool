c6a5e3345016d9cd1e00fa73227476917d08c955
Mateus Calza <mateushenrique17@gmail.com>
2020-10-01 20:36:27 -0300

    Canvas responsivo.

--- src/components/board/board.js
+++ src/components/board/board.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styled from 'styled-components'
+import { useMeasure } from 'react-use'
 
 const Wrapper = styled.main`
+  position: relative;
   display: flex;
   flex: 1;
   height: 100%;
+
+  canvas {
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: block;
+  }
 `
 
 export default function Board() {
-  return <Wrapper></Wrapper>
+  const canvasRef = useRef()
+  const [wrapperRef, { width, height }] = useMeasure()
+
+  return (
+    <Wrapper ref={wrapperRef}>
+      <canvas width={width} height={height} ref={canvasRef} />
+    </Wrapper>
+  )
 }
